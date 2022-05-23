990b4b614728dcc71562d02c4da247c13942a226
Profsain <profsaincrown@gmail.com>
2022-05-23 15:42:57 +0100

    Add Book section function

--- main.js
+++ main.js
@@ -1,16 +1,43 @@
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const addButton = document.querySelector('#add-button');
-const removeButton = document.querySelectorAll('.remove-button')
+const bookSection = document.querySelector('.book-section');
+let removeButton;
 
-const books = [];
+let books = [];
 
 function addBook() {
   let book = {
     title: title.value,
     author: author.value,
-    id: new Date().curre
+    id: Date.now()
   };
+  books.push(book);
+}
 
-  books.append(book);
+addButton.addEventListener('click', () => {
+  addBook();
+  generateBooks();
+  removeButton = document.querySelector('.remove-button');
+});
+
+// generate books
+const generateBooks = () => {
+  books.forEach((book) => {
+    const bookContainer = document.createElement('div');
+    bookContainer.innerHTML = `
+    <p>${book.title}</p>
+    <p>${book.author}</p>
+    <button class="remove-button" type="button" onclick="removeBooks(${book.id})">Remove</button><br>
+    <hr>  
+    `;
+    bookSection.appendChild(bookContainer);
+  });
+}
+
+// remove books
+const removeBooks = (id) => {
+  console.log(id)
+  let filterBook = books.filter(book => book.id === id);
+  console.log(filterBook.author);
 }
