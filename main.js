0ab3c1013bb15acd41f9953ac131995aeb743a88
Kamba56 <tomajohn316@yahoo.com>
2022-05-23 16:44:31 +0100

    Add remove function

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const addButton = document.querySelector('#add-button');
 const bookSection = document.querySelector('.book-section');
-let removeButton;
+let removeButton = [];
 
 let books = [];
 
@@ -13,31 +13,37 @@ function addBook() {
     id: Date.now()
   };
   books.push(book);
+  generateBooks(book.title, book.author, book.id);
+  button = document.getElementById(book.id.toString());
+  removeButton.push(button);
+  console.log(removeButton);
 }
 
 addButton.addEventListener('click', () => {
   addBook();
-  generateBooks();
-  removeButton = document.querySelector('.remove-button');
+  clearInputs();
 });
 
 // generate books
-const generateBooks = () => {
-  books.forEach((book) => {
-    const bookContainer = document.createElement('div');
-    bookContainer.innerHTML = `
-    <p>${book.title}</p>
-    <p>${book.author}</p>
-    <button class="remove-button" type="button" onclick="removeBooks(${book.id})">Remove</button><br>
-    <hr>  
-    `;
-    bookSection.appendChild(bookContainer);
-  });
+const generateBooks = (title, author, id) => {
+  const bookContainer = document.createElement('div');
+  bookContainer.innerHTML = `
+  <p>${title}</p>
+  <p>${author}</p>
+  <button class="remove-button" type="button" id="${id}" onclick="removeBooks(${id})">Remove</button><br>
+  <hr>  
+  `;
+  bookSection.appendChild(bookContainer);
+}
+
+const clearInputs = () => {
+  title.value = "";
+  author.value = "";
 }
 
 // remove books
 const removeBooks = (id) => {
-  console.log(id)
-  let filterBook = books.filter(book => book.id === id);
-  console.log(filterBook.author);
+    let filterBook = books.filter(book => book.id === id);
+    books.pop(filterBook[0]);
+    console.log(books);
 }
