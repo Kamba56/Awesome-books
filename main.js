bca1eb9c616dea84674f9174ce7dce88b086953d
Kamba56 <tomajohn316@yahoo.com>
2022-05-24 15:45:15 +0100

    Add styling

--- main.js
+++ main.js
@@ -3,22 +3,26 @@ const author = document.querySelector('#author');
 const form = document.querySelector('#add-form');
 const bookSection = document.querySelector('.book-section');
 
-// const books = JSON.parse(localStorage.getItem('books')) || [];
-
 // object class
 class BooksTemplate{
   books = [];
   constructor() {
 
   };
+
+  check() {
+    if(localStorage.getItem('books')) {
+      console.log("yesss");
+      this.books = JSON.parse(localStorage.getItem('books'));
+    }
+  }
   // methods
   generateBooks = ({ author, title, id }) => {
     const bookContainer = document.createElement('div');
+    bookContainer.classList.add('book-div');
     bookContainer.innerHTML = `
-    <p>${title}</p>
-    <p>${author}</p>
-    <button id="${id}" class="remove-button" type="button")">Remove</button><br>
-    <hr>  
+    <p>"${title}" by ${author}</p>
+    <button id="${id}" class="remove-button" type="button")">Remove</button>  
     `;
     bookSection.appendChild(bookContainer);
   };
@@ -29,10 +33,11 @@ class BooksTemplate{
       title: title.value,
       id: Date.now(),
     };
+    this.check();
     this.books.push(book);
-    console.log(this.books);
     localStorage.setItem('books', JSON.stringify(this.books));
     this.generateBooks(book);
+    console.log(this.books);
   };
 
   removeBook(removedBotton) {
@@ -43,29 +48,7 @@ class BooksTemplate{
     removedBook.remove();
   }
 }
-// generate books dynamically
-// const generateBooks = ({ author, title, id }) => {
-//   const bookContainer = document.createElement('div');
-//   bookContainer.innerHTML = `
-//   <p>${title}</p>
-//   <p>${author}</p>
-//   <button id="${id}" class="remove-button" type="button")">Remove</button><br>
-//   <hr>  
-//   `;
-//   bookSection.appendChild(bookContainer);
-// };
 
-// Add book
-// function addBook() {
-//   const book = {
-//     author: author.value,
-//     title: title.value,
-//     id: Date.now(),
-//   };
-//   books.push(book);
-//   localStorage.setItem('books', JSON.stringify(books));
-//   generateBooks(book);
-// }
 const bookObj = new BooksTemplate();
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -79,13 +62,8 @@ bookSection.addEventListener('click', (e) => {
   if (e.target.classList.contains('remove-button')) {
     const removedBotton = e.target;
     bookObj.removeBook(removedBotton);
-
-    // const removedBook = removedBotton.parentElement;
-    // const removedBookarr = books.filter((book) => book.id === Number(removedBotton.id));
-    // books.splice(books.indexOf(removedBookarr[0]), 1);
-    // localStorage.setItem('books', JSON.stringify(books));
-    // removedBook.remove();
   }
 });
 
-// bookObj.books.forEach(bookObj.generateBooks);
+bookObj.check();
+bookObj.books.forEach(bookObj.generateBooks);
