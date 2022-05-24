e46dec91628b367d9d52fb1d77c7beddffe1a504
toma <tomajohn316@yahoo.com>
2022-05-24 09:08:19 +0100

    Fix linter errors and add README.md file

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const author = document.querySelector('#author');
 const form = document.querySelector('#add-form');
 const bookSection = document.querySelector('.book-section');
 
-let books = JSON.parse(localStorage.getItem('books')) || [];
+const books = JSON.parse(localStorage.getItem('books')) || [];
 
 // generate books dynamically
-const generateBooks = ({author, title, id}) => {
+const generateBooks = ({ author, title, id }) => {
   const bookContainer = document.createElement('div');
   bookContainer.innerHTML = `
   <p>${title}</p>
@@ -15,15 +15,15 @@ const generateBooks = ({author, title, id}) => {
   <hr>  
   `;
   bookSection.appendChild(bookContainer);
-}
+};
 
 // Add book
 function addBook() {
-  let book = {
+  const book = {
     author: author.value,
     title: title.value,
-    id: Date.now()
-  }
+    id: Date.now(),
+  };
   books.push(book);
   localStorage.setItem('books', JSON.stringify(books));
   generateBooks(book);
@@ -31,17 +31,17 @@ function addBook() {
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  if(title.value && author.value) {
+  if (title.value && author.value) {
     addBook();
     form.reset();
   }
 });
 
 bookSection.addEventListener('click', (e) => {
-  if(e.target.classList.contains('remove-button')){
-    let removedBotton = e.target;
-    let removedBook = removedBotton.parentElement;
-    let removedBookarr = books.filter(book => book.id == removedBotton.id);
+  if (e.target.classList.contains('remove-button')) {
+    const removedBotton = e.target;
+    const removedBook = removedBotton.parentElement;
+    const removedBookarr = books.filter((book) => book.id === Number(removedBotton.id));
     books.splice(books.indexOf(removedBookarr[0]), 1);
     localStorage.setItem('books', JSON.stringify(books));
     removedBook.remove();
